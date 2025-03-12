6f173f32113b18986703192b205c3b7bd61ef528
vinceyap88 <vinceyap88@gmail.com>
2025-03-12 07:27:28 -0400

    Updated

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,7 @@
 """This module provides a function to run emotion detection."""
 
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     """Analyze the emaition based on the input text."""  
@@ -9,4 +10,10 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header, timeout=5)
-    return response.text
+    emotions = {}
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+
+    return emotions
